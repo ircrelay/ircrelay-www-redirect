230f33241b9cdbb6e3cb1d7925e5043d9c2ec9a9
Jack Pearkes <jackpearkes+git@gmail.com>
2012-11-01 22:26:53 -0700

    Redirect to www.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return redirect('https://ircrelay.com')
+    return redirect('http://www.ircrelay.com')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ['PORT'])
