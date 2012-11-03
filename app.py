f84eb6237f6246e00539916cf2e239b363707b25
Jack Pearkes <jackpearkes+git@gmail.com>
2012-11-03 16:52:47 -0700

    Add a catch-all redirect.

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def hello():
     return redirect('http://www.ircrelay.com')
 
+
+@app.route("/<slug>")
+def catch_all(slug):
+    return redirect('http://www.ircrelay.com/%s' % slug)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ['PORT'])
